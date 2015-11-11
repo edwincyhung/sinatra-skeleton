1d5e2a52a1a1ec4fc601b26adcd0ef3539bb02ea
Edwin <edwincyhung@Gmail.com>
2015-11-11 13:05:40 -0800

    add get and post to actions.erb
    Lighthouse Labs PT Web Fundamentals W5D1 Homework

--- app/actions.rb
+++ app/actions.rb
@@ -6,3 +6,23 @@ end
 get '/login' do
   erb :login
 end
+
+get '/profile' do
+  erb :profile
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/login' do
+  redirect '/'
+end
+
+post '/profile' do
+  redirect '/'
+end
+
+post 'signup' do
+  redirect '/'
+end
